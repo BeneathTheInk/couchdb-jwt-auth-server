1f6714d69cad37da7bb1251537568813da1e9a23
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-08 12:06:05 -0700

    fix: renewal tokens need session id

--- src/renew.js
+++ src/renew.js
@@ -2,7 +2,7 @@
 export default async function put(req, res, next) {
   try {
     const data = await req.app.validateToken(req.jwt, true);
-    const token = await req.app.generateToken(data);
+    const token = await req.app.generateToken(data, data.session);
 
     res.type('application/jwt').send(token);
   } catch(err) {
