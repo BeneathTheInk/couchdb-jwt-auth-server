a4d6436e0682fa574b7a743c689a2ba3ce8d5556
Tyler Johnson <tyler@tylerjohnson.me>
2016-03-08 17:43:09 -0700

    fix: post and put dates in ISO 8601 format

--- src/create-generate-token.js
+++ src/create-generate-token.js
@@ -3,11 +3,13 @@ import jwt from 'jsonwebtoken';
 export default function createGenerateToken({algorithms, expiresIn, secret}) {
   return function generateToken({name, roles}, session) {
     let token = jwt.sign({name, roles, session}, secret, {algorithms, expiresIn});
-    let {iat:issued,exp:expires} = jwt.decode(token);
+    let {iat,exp} = jwt.decode(token);
     return {
       ok: true,
       userCtx: { name, roles },
-      token, issued, expires, session
+      session, token,
+      issued: new Date(iat * 1000),
+      expires: new Date(exp * 1000)
     };
   };
 }
